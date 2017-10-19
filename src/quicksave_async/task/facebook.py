d8b11388401440bd6f1b8f2871db927a08222894
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-19 10:58:31 +0200

    samples

--- src/quicksave_async/task/facebook.py
+++ src/quicksave_async/task/facebook.py
@@ -16,6 +16,8 @@ def facebook_video(internal_create_request_bean, storage_provider):
         facebook_link = item_dir + '/facebook-video'
         youtube_dl_output = subprocess.check_output('youtube-dl %s' % meta.text, shell=True).decode()
         output_file = retrieve_from_string_by_regex(youtube_dl_output, r'Merging formats into "(.*)"')
+        sanitize_output_file = re.sub(r'[^a-zA-Z0-9\ _\-\.', '_', output_file)
+        output_file = sanitize_output_file
         subprocess.check_output(['ln', '-s', output_file, facebook_link])
         print(output_file)
         filesize = os.path.getsize(output_file)
